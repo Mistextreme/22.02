fd2eecf65c5a94906ac1405fb311fa895bd290d8
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2026-02-22 16:46:43 +0000

    Upload: fxmanifest.lua

---
+++ xeno-hackpanel/fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Xeno Shop'
+description 'Advanced Hacking Panel System with Multiple Mini-Games'
+version '1.0.0'
+
+ui_page 'web/dist/index.html'
+
+escrow_ignore {
+    'config.lua',
+}
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua',
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+files {
+    'web/dist/index.html',
+    'web/dist/**/*'
+}
+
+exports {
+    'StartHack'
+}
+
+server_exports {
+    'RegisterHackGame',
+    'UnregisterHackGame'
+}
+
+dependency '/assetpacks'
